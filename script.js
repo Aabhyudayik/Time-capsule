5bd195d1446ae77d6d42a9f18aa9a7732ffd871a
Aabhyudayik <aabhyu.sharma@gmail.com>
2026-02-15 22:02:29 +0545

    made it a little more interactive

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // @ts-nocheck
 let photo=document.getElementById('photo');
 let pic=document.getElementById("pic");
+let text=document.getElementById("text");
+let email=document.getElementById("email");
+let button=document.getElementById("button");
+let date=document.getElementById("date");
 photo.addEventListener("change", display);
 function display(){
     const reader= new FileReader();
@@ -8,9 +12,37 @@ function display(){
     reader.onload =function(){
         pic.src= reader.result;
     }
-   
+
+ let base64=pic.src;
+
+}
+pic.addEventListener("load",frame);
+function frame(){
+    pic.classList.add("polaroid")
+
+
+
+}
+pic.addEventListener("load",time);
+function time(){
+    date.style.display="block";
+}
+
+button.addEventListener("click",collect);
+function collect(){
+    const capsule_element={
+        
+        text:text.value,
+        email:email.value,
+        pic:base64.value,
+        date:date.value
+
+    }
+    console.log("Yes it worked");
+    
+    
 
 
 }
 
- 
+
