9642a630aa0df8d247f0bfc9c499fb8271d8972c
Aabhyudayik <aabhyu.sharma@gmail.com>
2026-03-26 22:19:00 +0545

    small change

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 dotenv.config()
 function connect(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>console.log("yas")).catch((error)=>console.log(error))
+    mongoose.connect(process.env.MONGO_URI).then(()=>console.log("yas")).catch(()=>console.log("no"))
 
 }
 export default connect
